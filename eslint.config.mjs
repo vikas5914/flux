a8c22f5a19bd105959c2e7804ff8773395d1475f
Vikas Kapadiya <vikas@kapadiya.net>
2026-03-02 01:03:56 +0530

    refactor: migrate from Next.js to Vinext and enable React Compiler
    - Update build scripts to use vinext commands
    - Add vinext/vite build directories to gitignore
    - Enable React Compiler in next.config.ts
    - Add eslint-plugin-react-you-might-not-need-an-effect

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import reactYouMightNotNeedAnEffect from "eslint-plugin-react-you-might-not-need-an-effect";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  reactYouMightNotNeedAnEffect.configs.recommended,
   {
     rules: {
       "@next/next/no-img-element": "off",

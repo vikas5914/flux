7c5f58eebc25ca18342b9996ab920a55f6b7da28
Vikas Kapadiya <vikas@kapadiya.net>
2026-03-03 17:52:23 +0530

    refactor: update vite and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import reactYouMightNotNeedAnEffect from "eslint-plugin-react-you-might-not-need-an-effect";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,6 +15,7 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      reactYouMightNotNeedAnEffect.configs.strict,
     ],
     languageOptions: {
       ecmaVersion: 2020,
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
+  plugins: [react(), tailwindcss(),VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
 

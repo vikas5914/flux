fa6063c0d5857e67f2c1233315dc02d731c99dc0
Vikas Kapadiya <vikas@kapadiya.net>
2026-03-07 05:09:04 +0530

    feat: integrate ConvexAuthProvider in root component for authentication support

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexReactClient } from "convex/react";
 import { Providers } from "./lib/query-client";
 import "./index.css";
 import App from "./App.tsx";
 
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Providers>
-        <App />
-      </Providers>
-    </BrowserRouter>
+    <ConvexAuthProvider client={convex}>
+      <BrowserRouter>
+        <Providers>
+          <App />
+        </Providers>
+      </BrowserRouter>
+    </ConvexAuthProvider>
   </StrictMode>,
 );
